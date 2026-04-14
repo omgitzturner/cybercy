62d16551cdd2bacef783ccf5da6fb8a1d9eacaf6
Eugene Turner III <eugene_turner_3@protonmail.com>
2026-04-13 21:33:31 -0400

    Fix CORS for production deployment

--- server/server.js
+++ server/server.js
@@ -27,8 +27,9 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? process.env.ALLOWED_ORIGINS?.split(',') || false
+    ? (process.env.ALLOWED_ORIGINS?.split(',') || ['https://cybercy.vercel.app'])
     : '*',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
